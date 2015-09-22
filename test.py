95e0e58b6f26fc428f2c0db160bf58d2cb8ac0e3
Justin Campana <justin.campana@gmail.com>
2015-09-21 20:59:26 -0700

    Minor changes

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-testtests
+#!/usr/bin/python
+
+import urllib3
+
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://thetvdb.com/api/58AE0E6345017543/series/79349/all')
+print(r.status)
+print(r.headers)
+print(r.data)
