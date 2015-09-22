0e39a850487a0c32bb54e90cb4811bc7a32dba97
slampana <justin.campana@gmail.com>
2015-09-22 11:16:14 -0700

    Misc. updates

--- test.py
+++ test.py
@@ -5,9 +5,6 @@ http = urllib3.PoolManager()
 r = http.request('GET', 'http://thetvdb.com/api/58AE0E6345017543/series/79349/all')
 xmldoc = minidom.parse(r)
 
-# for elements in xmldoc.getElementsByTagName('id'):
-#     print(elements.firstChild.nodeValue)
-
 for node in xmldoc.getElementsByTagName('Episode'):
     season = int(node.getElementsByTagName('SeasonNumber')[0].firstChild.data)
     episode = int(node.getElementsByTagName('EpisodeNumber')[0].firstChild.data)
@@ -15,6 +12,3 @@ for node in xmldoc.getElementsByTagName('Episode'):
         print(node.getElementsByTagName('id')[0].firstChild.data, "-", sep='', end='')
         print("S", season, "E", episode, "-", sep='', end='')
         print(node.getElementsByTagName('EpisodeName')[0].firstChild.data, sep='')
-
-
-
