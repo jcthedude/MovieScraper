25a68af1a19753fe67920bb8ee7149d6f05f8673
Justin Campana <justin.campana@gmail.com>
2015-09-27 16:11:23 -0700

    Minor changes

--- omdb_bulk_update_episode.py
+++ omdb_bulk_update_episode.py
@@ -2,6 +2,7 @@ import urllib3
 import mysql.connector as sql
 from xml.dom import minidom
 from xml.parsers.expat import ExpatError
+from datetime import datetime
 
 api_key = ""
 db_config = {
@@ -13,7 +14,7 @@ db_config = {
 }
 
 
-def db_insert(imdb_title, imdb_series_id, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type):
+def db_insert(imdb_title, imdb_series_id, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error):
     print("Starting on: ", imdb_id, "-", imdb_title)
     if imdb_votes == "N/A":
         imdb_votes = 0
@@ -21,14 +22,12 @@ def db_insert(imdb_title, imdb_series_id, imdb_id, imdb_rating, imdb_votes, imdb
         imdb_votes = None
     else:
         imdb_votes = int(str.replace(imdb_votes, ",", ""))
-
     if imdb_rating == "N/A":
         imdb_rating = 0
 
     connection = sql.connect(**db_config)
     cursor = connection.cursor()
-    cursor.execute("""INSERT INTO episode_staging_omdb SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1, CURRENT_TIMESTAMP""", (imdb_id, imdb_series_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type))
-
+    cursor.execute("""INSERT INTO episode_staging_omdb SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1, %s, CURRENT_TIMESTAMP""", (imdb_id, imdb_series_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error))
     connection.commit()
     print("Finished with: ", imdb_id, "-", imdb_title)
     cursor.close()
@@ -36,6 +35,7 @@ def db_insert(imdb_title, imdb_series_id, imdb_id, imdb_rating, imdb_votes, imdb
 
 
 def db_update():
+    start_time = datetime.now()
     print("Starting update...")
     connection = sql.connect(**db_config)
     cursor = connection.cursor()
@@ -44,6 +44,9 @@ def db_update():
     cursor.close()
     connection.close()
     print("UPDATE COMPLETE!!!: ")
+    end_time = datetime.now()
+    duration = end_time - start_time
+    print("Update duration (minutes): ", str(duration.seconds / 60))
 
 
 def get_omdb_data(imdb_id):
@@ -58,7 +61,11 @@ def get_omdb_data(imdb_id):
             if xml.getElementsByTagName("root")[0].getAttribute("response") == "True":
                 try:
                     imdb_title = xml.getElementsByTagName("movie")[0].getAttribute("title")
-                    imdb_series_id = xml.getElementsByTagName("movie")[0].getAttribute("seriesID")
+                    try:
+                        imdb_series_id = xml.getElementsByTagName("movie")[0].getAttribute("seriesID")
+                    except:
+                        pass
+                        imdb_series_id = None
                     imdb_rating = xml.getElementsByTagName("movie")[0].getAttribute("imdbRating")
                     imdb_votes = xml.getElementsByTagName("movie")[0].getAttribute("imdbVotes")
                     imdb_runtime = xml.getElementsByTagName("movie")[0].getAttribute("runtime")
@@ -70,38 +77,45 @@ def get_omdb_data(imdb_id):
                     imdb_poster = xml.getElementsByTagName("movie")[0].getAttribute("poster")
                     imdb_type = xml.getElementsByTagName("movie")[0].getAttribute("type")
 
-                    return imdb_title, imdb_series_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type
+                    return imdb_title, imdb_series_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, None
                 except IndexError as e:
                     print(imdb_id, "- INDEX ERROR: ", e)
-                    return None, None, None, None, None, None, None, None, None, None, None, None
+                    return None, None, None, None, None, None, None, None, None, None, None, None, str(e)
                     pass
             else:
-                print(xml.getElementsByTagName("error")[0].firstChild.data)
-                return None, None, None, None, None, None, None, None, None, None, None, None
+                e = xml.getElementsByTagName("error")[0].firstChild.data
+                print(e)
+                return None, None, None, None, None, None, None, None, None, None, None, None, str(e)
         except ExpatError as e:
             pass
             print(imdb_id, "- EXPAT ERROR: ", e)
-            return None, None, None, None, None, None, None, None, None, None, None, None
+            return None, None, None, None, None, None, None, None, None, None, None, None, str(e)
 
 
 def main_bulk_update():
     try:
+        start_time = datetime.now()
         connection = sql.connect(**db_config)
         cursor = connection.cursor()
-        # Add filters in this SELECT statement to determine the rows to be updated
-        cursor.execute("""SELECT DISTINCT imdbId FROM episode WHERE imdbId IS NOT NULL AND omdbFetched = 0""")
 
+        # Add filters in this SELECT statement to determine the rows to be updated
+        cursor.execute("""SELECT DISTINCT imdbId FROM episode WHERE imdbId IS NOT NULL AND omdbFetched = 0 LIMIT 100000""")
         rows = cursor.fetchall()
         for row in rows:
             imdb_id = row[0]
 
-            imdb_title, imdb_series_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type = get_omdb_data(imdb_id)
-            db_insert(imdb_title, imdb_series_id, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type)
+            imdb_title, imdb_series_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error = get_omdb_data(imdb_id)
+            db_insert(imdb_title, imdb_series_id, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error)
 
         print("INSERT COMPLETE!!!")
         db_update()
         cursor.close()
         connection.close()
+        end_time = datetime.now()
+        duration = end_time - start_time
+        print("Start time: ", str(start_time))
+        print("End time: ", str(end_time))
+        print("Total duration (minutes): ", str(duration.seconds / 60))
     except sql.Error as e:
         print("ERROR WITH SQL CONNECTION: ", e)
     except ExpatError as e:
--- omdb_bulk_update_series.py
+++ omdb_bulk_update_series.py
@@ -2,6 +2,7 @@ import urllib3
 import mysql.connector as sql
 from xml.dom import minidom
 from xml.parsers.expat import ExpatError
+from datetime import datetime
 
 api_key = ""
 db_config = {
@@ -13,24 +14,20 @@ db_config = {
 }
 
 
-def db_insert(imdb_title, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type):
+def db_insert(imdb_title, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error):
     print("Starting on: ", imdb_id, "-", imdb_title)
-
     if imdb_votes == "N/A":
         imdb_votes = 0
     elif imdb_votes is None:
         imdb_votes = None
     else:
         imdb_votes = int(str.replace(imdb_votes, ",", ""))
-
     if imdb_rating == "N/A":
         imdb_rating = 0
 
     connection = sql.connect(**db_config)
     cursor = connection.cursor()
-
-    cursor.execute("""INSERT INTO series_staging_omdb SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1, CURRENT_TIMESTAMP""", (imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type))
-
+    cursor.execute("""INSERT INTO series_staging_omdb SELECT %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 1, %s, CURRENT_TIMESTAMP""", (imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error))
     connection.commit()
     print("Finished with: ", imdb_id, "-", imdb_title)
     cursor.close()
@@ -38,6 +35,7 @@ def db_insert(imdb_title, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_y
 
 
 def db_update():
+    start_time = datetime.now()
     print("Starting update...")
     connection = sql.connect(**db_config)
     cursor = connection.cursor()
@@ -46,6 +44,9 @@ def db_update():
     cursor.close()
     connection.close()
     print("UPDATE COMPLETE!!!: ")
+    end_time = datetime.now()
+    duration = end_time - start_time
+    print("Update duration (minutes): ", str(duration.seconds / 60))
 
 
 def get_omdb_data(imdb_id):
@@ -71,40 +72,45 @@ def get_omdb_data(imdb_id):
                     imdb_poster = xml.getElementsByTagName("movie")[0].getAttribute("poster")
                     imdb_type = xml.getElementsByTagName("movie")[0].getAttribute("type")
 
-                    return imdb_title, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type
+                    return imdb_title, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, None
                 except IndexError as e:
                         print(imdb_id, "- INDEX ERROR: ", e)
-                        return None, None, None, None, None, None, None, None, None, None, None
+                        return None, None, None, None, None, None, None, None, None, None, None, str(e)
                         pass
             else:
-                print(xml.getElementsByTagName("error")[0].firstChild.data)
-                return None, None, None, None, None, None, None, None, None, None, None
+                e = xml.getElementsByTagName("error")[0].firstChild.data
+                print(e)
+                return None, None, None, None, None, None, None, None, None, None, None, str(e)
         except ExpatError as e:
             pass
             print(imdb_id, "- EXPAT ERROR: ", e)
-            return None, None, None, None, None, None, None, None, None, None, None
+            return None, None, None, None, None, None, None, None, None, None, None, str(e)
 
 
 def main_bulk_update():
     try:
+        start_time = datetime.now()
         connection = sql.connect(**db_config)
         cursor = connection.cursor()
 
         # Add filters in this SELECT statement to determine the rows to be updated
         cursor.execute("""SELECT DISTINCT imdbId FROM series WHERE imdbId IS NOT NULL AND omdbFetched = 0""")
-
         rows = cursor.fetchall()
-
         for row in rows:
             imdb_id = row[0]
 
-            imdb_title, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type = get_omdb_data(imdb_id)
-            db_insert(imdb_title, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type)
+            imdb_title, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error = get_omdb_data(imdb_id)
+            db_insert(imdb_title, imdb_id, imdb_rating, imdb_votes, imdb_runtime, imdb_year, imdb_genre, imdb_plot, imdb_country, imdb_awards, imdb_poster, imdb_type, omdb_error)
 
         print("INSERT COMPLETE!!!")
         db_update()
         cursor.close()
         connection.close()
+        end_time = datetime.now()
+        duration = end_time - start_time
+        print("Start time: ", str(start_time))
+        print("End time: ", str(end_time))
+        print("Total duration (minutes): ", str(duration.seconds / 60))
     except sql.Error as e:
         print("ERROR WITH SQL CONNECTION: ", e)
     except ExpatError as e:
