20553331d4657527ceee81d6ed75835afb122044
slampana <justin.campana@gmail.com>
2015-10-01 15:09:49 -0700

    Minor changes

--- imdb_scrape_series_list.py
+++ imdb_scrape_series_list.py
@@ -17,16 +17,16 @@ def db_insert_imdb_series_list(cursor, count, id, name):
 
 
 def imdb_fetch_series_list():
-    record_count = 100001
+    record_count = 1
     status = 200
-    count = 100001
+    count = 1
 
     connection = sql.connect(**db_config)
     cursor = connection.cursor()
 
     while status == 200:
-        url = "http://www.imdb.com/search/title?count=250&sort=num_votes&start=" + str(record_count) +\
-              "&title_type=tv_series&view=simple"
+        url = "http://www.imdb.com/search/title?languages=en|1&count=250&title_type=tv_series&sort=moviemeter,asc" \
+              "&view=simple&start=" + str(record_count)
         http = urllib3.PoolManager()
         r = http.request('GET', url)
         print(url)
@@ -34,15 +34,17 @@ def imdb_fetch_series_list():
 
         soup = BeautifulSoup(r.data, 'html.parser')
         rows = soup.find_all("td", class_="title")
-        print(rows)
 
+        if not rows:
+            print("No rows returned.")
+            break
         for row in rows:
             print(count)
             row_content = row.contents[0]
             name = row_content.contents[0]
             link = row_content.get('href')
             id = link[7:-1]
-            # db_insert_imdb_series_list(cursor, count, str(id), str(name))
+            db_insert_imdb_series_list(cursor, count, str(id), str(name))
             count += 1
 
         connection.commit()
@@ -51,7 +53,7 @@ def imdb_fetch_series_list():
     connection.commit()
     cursor.close()
     connection.close()
-    print(r.status)
+    print("Status: ", r.status)
 
 
 imdb_fetch_series_list()
