6fca5da5f4a3f7071527c97bc9088cf586968fd1
Justin Campana <justin.campana@gmail.com>
2015-09-30 21:34:33 -0700

    Minor changes

--- imdb_scrape_series_list.py
+++ imdb_scrape_series_list.py
@@ -1,34 +1,59 @@
 import urllib3
 from bs4 import BeautifulSoup
+import mysql.connector as sql
+
+db_config = {
+  'user': 'slampana',
+  'password': 'Campana1',
+  'host': '107.170.244.175',
+  'database': 'tvdb',
+  'raise_on_warnings': True,
+}
 
 
-def main():
-    record_count = 1
+def db_insert_imdb_series_list(cursor, count, id, name):
+    cursor.execute("""INSERT INTO imdb_series_list SELECT %s, %s, %s""", (count, id, name))
+    print("Insert complete: ", id, name)
+
+
+def imdb_fetch_series_list():
+    record_count = 100001
     status = 200
+    count = 100001
 
-    while record_count < 1000 and status == 200:
+    connection = sql.connect(**db_config)
+    cursor = connection.cursor()
+
+    while status == 200:
         url = "http://www.imdb.com/search/title?count=250&sort=num_votes&start=" + str(record_count) +\
               "&title_type=tv_series&view=simple"
         http = urllib3.PoolManager()
         r = http.request('GET', url)
-
+        print(url)
         status = r.status
 
         soup = BeautifulSoup(r.data, 'html.parser')
-        # id = soup.find("td", {"class": "title"}).h6.contents
         rows = soup.find_all("td", class_="title")
-        for row in rows:
-            # print(row)
-            row_content = row.contents[0]
-            title = row_content.contents[0]
-            link = row_content.get('href')
-            print(title, link[7:-1])
+        print(rows)
 
+        for row in rows:
+            print(count)
+            row_content = row.contents[0]
+            name = row_content.contents[0]
+            link = row_content.get('href')
+            id = link[7:-1]
+            # db_insert_imdb_series_list(cursor, count, str(id), str(name))
+            count += 1
+
+        connection.commit()
         record_count += 250
 
-    print("Record count: ", record_count)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print(r.status)
 
 
-main()
+imdb_fetch_series_list()
 
 
