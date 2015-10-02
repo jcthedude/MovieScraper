08ab0d05ab12d408989370a7b49af2c180471754
slampana <justin.campana@gmail.com>
2015-10-02 09:04:36 -0700

    Added error handling for bad URL data

--- imdb_scrape_series_season_list.py
+++ imdb_scrape_series_season_list.py
@@ -16,7 +16,7 @@ def db_select_imdb_series_list():
         print("Fetching  all series...")
         connection = sql.connect(**db_config)
         cursor = connection.cursor()
-        cursor.execute("""SELECT DISTINCT id FROM imdb_series_list ORDER BY row""")
+        cursor.execute("""SELECT DISTINCT id FROM imdb_series_list WHERE row >= 4476 ORDER BY row""")
         rows = cursor.fetchall()
         cursor.close()
         connection.close()
@@ -39,35 +39,46 @@ def imdb_fetch_series_season_list():
 
     for row in rows:
         series_id = row[0]
+        valid_url = True
         url = "http://www.imdb.com/title/" + series_id
         http = urllib3.PoolManager()
-        r = http.request('GET', url)
+
+        try:
+            r = http.request('GET', url)
+        except:
+            valid_url = False
+            print("Problem with URL data returned.")
+            pass
+
         print(count, url)
 
-        soup = BeautifulSoup(r.data, 'html.parser')
-        rows = soup.find_all("div", class_="seasons-and-year-nav")
+        if valid_url:
+            soup = BeautifulSoup(r.data, 'html.parser')
+            rows = soup.find_all("div", class_="seasons-and-year-nav")
 
-        if len(rows) != 0:
-            for row in rows:
-                links = row.findAll('a')
-                for link in links:
-                    if "season" in str(link):
-                        season_number = str(link.string)
-                        url = str(link['href'])
-                        print(season_number, url)
-                        if season_number.isdigit():
-                            db_insert_imdb_series_season_list(cursor, str(series_id), season_number, url)
+            if len(rows) != 0:
+                for row in rows:
+                    links = row.findAll('a')
+                    for link in links:
+                        if "season" in str(link):
+                            season_number = str(link.string)
+                            url = str(link['href'])
+                            print(season_number, url)
+                            if season_number.isdigit():
+                                db_insert_imdb_series_season_list(cursor, str(series_id), season_number, url)
 
-            connection.commit()
-            print("Insert complete: ", series_id)
-            count += 1
+                connection.commit()
+                print("Insert complete: ", series_id)
+                count += 1
+            else:
+                print("No seasons found for: ", series_id)
+                count += 1
         else:
-            print("No seasons found for: ", series_id)
             count += 1
 
     cursor.close()
     connection.close()
-    print("Process complete. ", count, "rows inserted.")
+    print("Process complete. ", count-1, "series processed.")
 
 
 imdb_fetch_series_season_list()
