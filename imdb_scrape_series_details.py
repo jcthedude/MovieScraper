760b1fcba1da89d3076b56511d493209e190ce91
Justin Campana <justin.campana@gmail.com>
2015-10-04 18:39:19 -0700

    Minor updates

--- imdb_scrape_series_details.py
+++ imdb_scrape_series_details.py
@@ -11,13 +11,9 @@ collection_show = db.show
 
 def db_select_imdb_series_list():
     print("Fetching  all series...")
-    id_list = collection_show_list.find({}, {'id': 1, 'name': 1, 'order': 1, '_id': 0}).sort([("order", 1)])
+    id_list = collection_show_list.find({"order": {"$gt": 198}}, {'id': 1, 'name': 1, 'order': 1, '_id': 0}).sort([("order", 1)])
 
     return id_list
-
-
-# def db_insert_imdb_series_season_list(cursor, id, seasonNumber, url):
-#     cursor.execute("""INSERT INTO imdb_series_season_list SELECT %s, %s, %s""", (id, seasonNumber, url))
 
 
 def imdb_fetch_series_season_list():
@@ -42,50 +38,94 @@ def imdb_fetch_series_season_list():
             print("Problem with URL data returned.")
             pass
 
-        print(count, url)
+        print(order, url)
 
         if valid_url:
             show = {}
             show.update({"id": show_id, "name": name, "order": order, "timestamp": timestamp})
 
             soup = BeautifulSoup(r.data, 'html.parser')
-            soup_image = soup.find_all("div", {"class": "image"})[0].find_all('a')[0].find_all('img')[0]['src']
-            soup_description = soup.find_all("p", {"itemprop": "description"})[0]
-            soup_content_rating = soup.find_all("span", {"itemprop": "contentRating"})[0]
-            soup_runtime = soup.find_all("time", {"itemprop": "duration"})[0]
-            soup_creator = soup.find_all("div", {"itemprop": "creator"})[0].find_all('a')
-            soup_rating = soup.find_all("span", {"itemprop": "ratingValue"})[0]
-            soup_rating_count = soup.find_all("span", {"itemprop": "ratingCount"})[0]
-            soup_genre = soup.find_all("div", {"itemprop": "genre"})[0].find_all('a')
-            soup_rundate = soup.find_all("span", {"class": "nobr"})[0]
-            soup_recommended = soup.find_all("div", {"class": "rec_slide"})[0].find_all('a')
-            soup_cast = soup.find_all("table", {"class": "cast_list"})[0]
+            try:
+                soup_image = soup.find_all("div", {"class": "image"})[0].find_all('a')[0].find_all('img')[0]['src']
+            except IndexError:
+                soup_image = None
+                pass
+            try:
+                soup_description = soup.find_all("p", {"itemprop": "description"})[0]
+            except IndexError:
+                soup_description = None
+                pass
+            try:
+                soup_content_rating = soup.find_all("span", {"itemprop": "contentRating"})[0]
+            except IndexError:
+                soup_content_rating = None
+                pass
+            try:
+                soup_runtime = soup.find_all("time", {"itemprop": "duration"})[0]
+            except IndexError:
+                soup_runtime = None
+                pass
+            try:
+                soup_creator = soup.find_all("div", {"itemprop": "creator"})[0].find_all('a')
+            except IndexError:
+                soup_creator = None
+                pass
+            try:
+                soup_rating = soup.find_all("span", {"itemprop": "ratingValue"})[0]
+            except IndexError:
+                soup_rating = None
+                pass
+            try:
+                soup_rating_count = soup.find_all("span", {"itemprop": "ratingCount"})[0]
+            except IndexError:
+                soup_rating_count = None
+                pass
+            try:
+                soup_genre = soup.find_all("div", {"itemprop": "genre"})[0].find_all('a')
+            except IndexError:
+                soup_genre = None
+                pass
+            try:
+                soup_rundate = soup.find_all("span", {"class": "nobr"})[0]
+            except IndexError:
+                soup_rundate = None
+                pass
+            try:
+                soup_recommended = soup.find_all("div", {"class": "rec_slide"})[0].find_all('a')
+            except IndexError:
+                soup_recommended = None
+                pass
+            try:
+                soup_cast = soup.find_all("table", {"class": "cast_list"})[0]
+            except IndexError:
+                soup_cast = None
+                pass
 
-            if len(soup_image) != 0:
+            if soup_image is not None:
                 image = soup_image
                 show.update({'image': image})
             else:
                 print("No image found")
 
-            if len(soup_description) != 0:
+            if soup_description is not None:
                 description = soup_description.get_text().strip()
                 show.update({'description': description})
             else:
                 print("No description found")
 
-            if len(soup_content_rating) != 0:
+            if soup_content_rating is not None:
                 content_rating = soup_content_rating.get_text().strip()
                 show.update({'content_rating': content_rating})
             else:
                 print("No content rating found")
 
-            if len(soup_runtime) != 0:
+            if soup_runtime is not None:
                 runtime = soup_runtime.get_text().strip()
                 show.update({'runtime': runtime})
             else:
                 print("No runtime found")
 
-            if len(soup_creator) != 0:
+            if soup_creator is not None:
                 soup_count = 1
                 creator_list = []
                 for creator in soup_creator:
@@ -98,19 +138,19 @@ def imdb_fetch_series_season_list():
             else:
                 print("No creator found")
 
-            if len(soup_rating) != 0:
+            if soup_rating is not None:
                 rating = soup_rating.get_text().strip()
                 show.update({'rating': rating})
             else:
                 print("No rating found")
 
-            if len(soup_rating_count) != 0:
+            if soup_rating_count is not None:
                 rating_count = soup_rating_count.get_text().strip()
                 show.update({'rating_count': rating_count})
             else:
                 print("No rating count found")
 
-            if len(soup_genre) != 0:
+            if soup_genre is not None:
                 soup_count = 1
                 genre_list = []
                 for genre in soup_genre:
@@ -119,9 +159,9 @@ def imdb_fetch_series_season_list():
                     soup_count += 1
                 show.update({"genre": genre_list})
             else:
-                print("No actor found")
+                print("No genre found")
 
-            if len(soup_rundate) != 0:
+            if soup_rundate is not None:
                 rundate = soup_rundate.get_text().strip()
                 show.update({'rundate': rundate})
                 if " " in rundate:
@@ -132,25 +172,29 @@ def imdb_fetch_series_season_list():
             else:
                 print("No run date found")
 
-            if len(soup_recommended) != 0:
+            if soup_recommended is not None:
                 soup_count = 1
                 recommendation_list = []
                 for rec in soup_recommended:
-                    recommend_name = rec.find_all('img')[0]['alt']
-                    recommend_id = rec['href'][7:-17]
                     try:
-                        recommend_image = rec.find_all('img')[0]['loadlate']
-                    except:
-                        recommend_image = rec.find_all('img')[0]['src']
-                    recommendation_dict = ({"id": recommend_id, "name": recommend_name, "order": soup_count
-                                            , "image": recommend_image})
-                    recommendation_list.append(recommendation_dict)
-                    soup_count += 1
+                        recommend_name = rec.find_all('img')[0]['alt']
+                        recommend_id = rec['href'][7:-17]
+                        try:
+                            recommend_image = rec.find_all('img')[0]['loadlate']
+                        except:
+                            recommend_image = rec.find_all('img')[0]['src']
+                        recommendation_dict = ({"id": recommend_id, "name": recommend_name, "order": soup_count
+                                                , "image": recommend_image})
+                        recommendation_list.append(recommendation_dict)
+                        soup_count += 1
+                    except IndexError as e:
+                        print("IndexError: ", e)
+                        pass
                 show.update({"recommendation": recommendation_list})
             else:
                 print("No recommendations found")
 
-            if len(soup_cast) != 0:
+            if soup_cast is not None:
                 soup_count = 1
                 actor = []
                 for cast in soup_cast.find_all("td", {"class": "primary_photo"}):
@@ -179,12 +223,11 @@ def imdb_fetch_series_season_list():
                     actor_details_list.append(actor_details)
                     i += 1
                 show.update({"cast": actor_details_list})
-                print(actor_details_list)
             else:
                 print("No cast found")
 
-            print(show)
             collection_show.insert(show)
+            count += 1
 
         else:
             count += 1
