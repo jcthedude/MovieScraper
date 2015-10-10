ecc4235e12f57e7a3bc071ff33fe5bd872d8338b
Justin Campana <justin.campana@gmail.com>
2015-10-10 09:52:54 -0700

    Added KeyError checking for show and episode image scraping

--- imdb_scrape_episode_details.py
+++ imdb_scrape_episode_details.py
@@ -12,7 +12,7 @@ collection_show = db.show
 def db_select_imdb_show_list():
     # fetch the list of all shows to get details for
     print("Fetching all shows...")
-    id_list = collection_show.find({"order": {"$gt": 23297}}, {'id': 1, 'order': 1, '_id': 0}).sort([("order", 1)])
+    id_list = collection_show.find({"order": {"$gt": 0}}, {'id': 1, 'order': 1, '_id': 0}).sort([("order", 1)])
 
     return id_list
 
