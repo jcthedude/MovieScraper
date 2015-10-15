3feac311b793f2d555071fb91efe1e1641fb8123
Justin Campana <justin.campana@gmail.com>
2015-10-14 20:30:31 -0700

    Added bootstrap

--- web.py
+++ web.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def get_shows():
-    _shows = collection_show.find({"order": {"$lte": 100}}, {'id': 1, 'name': 1, 'order': 1, '_id': 0}).sort([("order", 1)])
+    _shows = collection_show.find({"order": {"$lte": 100}}, {'id': 1, 'name': 1, 'order': 1, 'banner': 1, 'rating': 1, '_id': 0}).sort([("order", 1)])
     shows = [show for show in _shows]
 
     return render_template('tv.html', shows=shows)
